ae42df547e68f2b08321f6376b6f64a2ef5c12cd
kristinavenevtseva <63412659+kristinavenevtseva@users.noreply.github.com>
2020-06-15 23:13:34 +0300

    modified PostManager

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -6,7 +6,7 @@ public class PostManager {
 
     private Post[] posts;
 
-    public Post[] search(int ownerId, String domain, String query, int count, int offset) {
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
 
         return null;
     }
